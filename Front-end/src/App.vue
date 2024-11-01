748820805322f279c62b4164e67613b9fcf047be
purin <purin.peera@mail.kmutt.ac.th>
2024-11-01 13:48:51 +0700

    nav bar from bronze

---
+++ Front-end/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import NavbarDropDown from './components/NavbarDropDown.vue';
+import Home from './views/Home.vue';
+</script>
+ 
+<template>
+    <Navbar class=" navbar
+    2xl:h-16 2xl:flex
+    lg:h-16 lg:flex
+    md:hidden
+    sm:hidden
+    "
+     />
+     <NavbarDropDown class="
+    2xl:hidden
+    lg:hidden
+    md:flex
+    sm:flex
+    "/>
+
+        <div class="all  mt-12 
+        2xl:ml-64 2xl:mr-64
+        xl:ml-64 xl:mr-64 
+        md:ml-28 md:mr-28
+        sm:ml-28 sm:mr-28 
+        ">
+            <Home />
+        </div>
+    
+</template>
+ 
+<style scoped>
+/* Hide the Navbar component on screens smaller than 640px */
+@media (max-width: 768px) {
+  .navbar {
+    display: none; /* Hides Navbar */
+  }
+}
+
+@media (max-width: 639px) {
+  .all {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+}
+</style>
