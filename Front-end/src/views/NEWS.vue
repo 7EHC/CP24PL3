e67cb5fd69a64d80482ab8a570b5960441673a44
purin <purin.peera@mail.kmutt.ac.th>
2025-02-10 07:05:53 +0700

    sort published date

--- Front-end/src/views/NEWS.vue
+++ Front-end/src/views/NEWS.vue
@@ -27,7 +27,9 @@ const shuffleNews = () => {
   const shuffled = [...news.value].sort(() => 0.5 - Math.random());
   firstFourNews.value = shuffled.slice(0, 4);
   lastFourNews.value = shuffled.slice(5, 9);
-  lastFourNews.value = lastFourNews.value.sort((a, b) => new Date(b.published_utc) -new Date(a.published_utc))
+  lastFourNews.value = lastFourNews.value.sort(
+    (a, b) => new Date(b.published_utc) - new Date(a.published_utc)
+  );
 };
 
 const nextSlide = () => {
@@ -124,7 +126,7 @@ const refresh = async () => {
   news.value = await fetchNews();
   itemsToShow.value = 15;
   isSearch.value = false;
-  shuffleNews()
+  shuffleNews();
 };
 
 onMounted(async () => {
