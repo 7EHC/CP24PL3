7b3547e5b0aafe1789549e4d09ab2df7a2aaed7d
PatKunc <102203214+PatKunc@users.noreply.github.com>
2025-02-15 17:50:20 +0700

    DropDownNavbar

--- Front-end/src/components/NavbarDropDown.vue
+++ Front-end/src/components/NavbarDropDown.vue
@@ -47,6 +47,7 @@ const toggleDropdown = () => {
       <RouterLink :to="{name:'NEWS'}" class="hover:text-yellow-500 duration-200">NEWS</RouterLink>
       <RouterLink :to="{name:'Calculator'}" class="hover:text-yellow-500 duration-200">DCA CALCULATOR</RouterLink>
       <RouterLink :to="{name:'Port'}" class="hover:text-yellow-500 duration-200">PORTFOLIO</RouterLink>
+      <RouterLink :to="{name:'History'}" class="hover:text-yellow-500 duration-200">HISTORY</RouterLink>
       <RouterLink :to="{name:'Login'}" class="border border-yellow-500 text-white px-4 py-1 rounded-full hover:bg-yellow-500 hover:text-black transition-colors duration-200 w-full text-left">
         LOGIN
       </RouterLink>
