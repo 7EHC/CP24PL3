4d4bea44ba44004ddc02ed7be7a6b893de477665
Purin <purin.peera@mail.kmutt.ac.th>
2025-02-17 10:39:23 +0700

    Merge branch 'purin' of https://github.com/7EHC/CP24PL3 into main

--- Back-end/routes/stock.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllTickers, searchTickers } from "../controllers/stock.controller.js";
-
-const router = Router();
-
-router.get("/allticker", getAllTickers);
-router.get("/searchTickers/:identifier", searchTickers);
-
-export default router;
